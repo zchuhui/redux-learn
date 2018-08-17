d14025d34ec0ad160e721d2827dd3a459fb26b65
zchuhui <943339564@qq.com>
2018-08-17 11:15:03 +0800

    add router and todos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import store from './js/store/index'
-import App from './js/components/App'; 
+import App from './App'; 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
---
+++ src/js/routes/todos.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TodosPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (<div>
+      todos
+    </div>);
+  }
+}
+
+export default TodosPage;
